7c476d6946defde88a836d663b128b19e4953c65
lycho33 <cho.lyds@gmail.com>
2021-04-06 17:31:26 -0400

    commit tree e87e25811b0c87116cfe84c43d3a8b4b5aaa752a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
+  before_action :set_students, only: :show
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  private
+  def set_students 
+    @students = Student.find_by(params[:id])
   end
 end
